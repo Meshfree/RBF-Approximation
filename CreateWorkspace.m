52151ff8c7e619294877a2eb06790e6695e60407
amirkrasnic <amir.krasnic@gmail.com>
2016-12-05 17:45:53 +0100

    added halton corner points

--- CreateWorkspace.m
+++ CreateWorkspace.m
@@ -7,5 +7,3 @@ filename = 'data/haltonPoints.mat'
 haltons = [500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000];
 
 createHaltonPoints(haltons, filename);
-
-load
--- utils/createHaltonPoints.m
+++ utils/createHaltonPoints.m
@@ -3,7 +3,7 @@ function createHaltonPoints(points, filename)
 narginchk(2,2);
 
 for i=1:length(points)
-    eval(sprintf('haltonPoints%d = haltonseq(points(i),2);', points(i)));
+    eval(sprintf('haltonPoints%d = haltonWithCorners(points(i),2);', points(i)));
 end
 
 save(filename, 'haltonPoints*');
