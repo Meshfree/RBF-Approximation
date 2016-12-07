69560129360619533e83b3dea212e7c847e1a6bf
amirkrasnic <amir.krasnic@gmail.com>
2016-12-07 14:16:20 +0100

    added haltonpoint sets with edge points

--- dataCreation/createHaltonPoints.m
+++ dataCreation/createHaltonPoints.m
@@ -5,6 +5,8 @@ function createHaltonPoints(points, filename)
     for i=1:length(points)
         eval(sprintf('haltonPoints%d = haltonWithCorners(points(i),2);', points(i)));
     end
-
+    eval(sprintf('haltonPointsEdges%d = haltonWithEdgesAndCorners(points(1),2);', points(1)));
+    eval(sprintf('haltonPointsEdges%d = haltonWithEdgesAndCorners(points(length(points)),2);', points(length(points))));
+    
     save(filename, 'haltonPoints*');
 end
