e989430e79b113ce1bdffd37f9486ea05545aeb2
amirkrasnic <amir.krasnic@gmail.com>
2016-12-07 18:07:48 +0100

    creation script tweaks

--- CreateWorkspace.m
+++ CreateWorkspace.m
@@ -8,10 +8,10 @@ finenameCenter = 'data/centerPoints.mat';
 fimenameUnit = 'data/unitGridPoints.mat';
 filenameEpsilon = 'data/epsilonPoints.mat';
 haltons = [500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000,4225];
-centers = 9;
+centers = 81;
 unitGrids = [];
-epsilon = [];
+epsilon = [500];
 
 createHaltonPoints(haltons, filenameHalton);
 createCenterPoints(centers, finenameCenter);
-% createEpsilonPoints(epsilon, filenameEpsilon);
+createEpsilonPoints(epsilon, filenameEpsilon);
--- utils/unitGrid.m
+++ utils/unitGrid.m
@@ -1,4 +1,5 @@
 function ret = unitGrid(count)
+count = floor(sqrt(count));
 grid = linspace(0,1,count);
 [xe,ye] = meshgrid(grid);
 ret =[xe(:) ye(:)];
