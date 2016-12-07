40ec8e08e6de43f70437f3a41664a8e1656c75f0
Stefan Ronczka (zumbam) <stefanronczka@outlook.de>
2016-12-07 16:41:14 +0100

    adding fixed edges script

--- ResultsWithFixedEdges.m
+++ ResultsWithFixedEdges.m
@@ -1,14 +1,10 @@
-close all;
 clear variables;
+close all;
 clc;
 addpath(genpath(pwd));
 alpha =5;
 rbfG = @(r) exp(-(alpha*r).^2); 
 rbfIQ = @(r) 1./(1 + (alpha* r).^2);
-
-x=linspace(0,1000,100);
-figure;
-plot(x,rbfIQ(x));
 
 load('centerPoints.mat')
 load('haltonPoints.mat')
@@ -21,30 +17,21 @@ gridY = linspace(0,1,neval);
 
 epoints = [xe(:) ye(:)];
 [pf,pf_p,E,exact] = CalculateImpactOfEvaluationPoints(@franke, rbfG, haltonPoints500, unit2DM81,epoints);
+[pf_e,pf_p_e,E_e,exact_e] = CalculateImpactOfEvaluationPoints(@franke, rbfG, haltonPointsEdges500, unit2DM81,epoints);
 
 % figure;
 % surf(xe,ye,reshape(exact,neval,neval));
 % hold on;
 % surf(xe*1000,ye*1000,reshape(pf_p,neval,neval))
-maxError =max(E(1),E(3));
-
-errorPfandPf_p = abs(pf_p -exact)- abs(pf-exact);
-figure;
-load('colormap')
-surf(xe,ye,reshape(errorPfandPf_p,neval,neval));
-colormap (mycmap)
-
-
+maxError =max(E(1),E_e(1));
 
 figure;
 fview = [100,30]; % viewing angles for plot
 subplot(2,2,1);
-PlotSurf(xe,ye,pf,neval,exact,maxError,fview);
-subplot(2,2,2);
-PlotSurf(xe,ye,pf_p,neval,exact,maxError,fview);
-subplot(2,2,3);
 PlotError2D(xe,ye,pf,exact,maxError,neval,fview);
-subplot(2,2,4);
-PlotError2D(xe,ye,pf_p,exact,maxError,neval,fview);
+subplot(2,2,2);
+PlotError2D(xe,ye,pf_e,exact_e,maxError,neval,fview);
+subplot(2,2,3);
+surf(xe,ye,reshape(abs(pf-pf_e),neval,neval));
 
 
